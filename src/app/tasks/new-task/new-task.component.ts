fda6f485e37016556e6e86476e4e2fbeb8b7e1fd
Laura <poleuca.laura@gmail.com>
2025-03-31 22:13:53 +0300

    use shared modules

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -12,8 +12,7 @@ import { TasksService } from '../tasks.service';
 
 @Component({
     selector: 'app-new-task',
-    standalone: true,
-    imports: [FormsModule],
+    standalone: false,
     templateUrl: './new-task.component.html',
     styleUrl: './new-task.component.css',
 })
